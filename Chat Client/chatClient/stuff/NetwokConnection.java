3fe5ed870d34160b5d0b17eea820b33058365bb8
167504 <167504@HSS-101-15.westwindsor.k12.nj.us>
2014-11-13 13:03:04 -0500

    Working on Network Connection

--- Chat Client/chatClient/stuff/NetwokConnection.java
+++ Chat Client/chatClient/stuff/NetwokConnection.java
@@ -1,5 +1,29 @@
 package stuff;
 
-public class NetwokConnection {
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class NetwokConnection {
+	
+	private Socket socket;
+	private Scanner in;
+	private PrintWriter out;
+	
+	public NetwokConnection(String host, int port) throws IOException
+	{
+		socket = new Socket(host, port);
+		InputStream inStream = socket.getInputStream();
+		OutputStream outStream = socket.getOutputStream();
+		
+		in = new Scanner(inStream);
+		out = new PrintWriter(outStream);
+	}
+	
+	
+	
+	public void destructor () throws IOException
+	{
+		socket.close();
+	}
 }

a4f92ac09caa08e7e40b9d61cd7fb195d0b14b7d
fjoe <francisfull@gmail.com>
2014-11-13 13:04:54 -0500

    Added frame

--- Chat Client/chatClient/stuff/Window.java
+++ Chat Client/chatClient/stuff/Window.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 
 public class Window 
 {
+	private JFrame frame;
 	private JTextArea userText;
 	private JLabel chat;
 	private JButton textButton;
 	
 	public Window()
 	{
-		
+		userText = new JTextArea();
 	}
 }
